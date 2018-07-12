d3df2befac6066a53dfb15b9bb532cd9649deb36
sadecki558 <matthewsadecki@gmail.com>
2018-07-12 18:15:41 -0400

    initial

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+
+	/*  Hamburger Menu & Icon  */
+	$('.hamburger').on('click', function(e){
+		
+		e.preventDefault();
+		$(this).toggleClass('opned');
+		$('header nav').toggleClass('active');
+		
+	});
+
+});
